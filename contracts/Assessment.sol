16175e130d40addeca3501e3f5185d4cda4bf5cf
MokshithPutta <133254208+MokshithPutta@users.noreply.github.com>
2023-10-30 23:13:06 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,14 +4,11 @@ pragma solidity ^0.8.9;
 //import "hardhat/console.sol";
 
 contract Assessment {
-    address payable public owner;
+
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
 
     constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
         balance = initBalance;
     }
 
@@ -19,42 +16,27 @@ contract Assessment {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+    function array_sum(uint[] memory array) public payable {
 
         // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        assert(array.length>0);
+        uint256 result  = 0;
+        for(uint i=0 ; i<array.length ; i++){
+            result += array[i];
+        }
+        balance  = result;
     }
 
     // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+    function array_product(uint[] memory array) public {
+ 
+        assert(array.length>0);
+        uint256 result = 1;
+        for(uint i=0 ; i<array.length ; i++){
+            result *= array[i];
         }
+        balance = result ;
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
     }
 }
